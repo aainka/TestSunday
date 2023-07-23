ad5d1cd1e4b5d0dc6ff43c77194d2a34dd649c37
jsjin01 <ta4723@naver.com>
2023-07-23 20:46:14 +0900

    블록 깨기 게임 1일차
    블록 내려오는 거 설계

---
+++ Block Break game(sangjin)/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+//red block size 59*25
+//blue ball size 10*10 45도 회전
+
+namespace Block_Break_Game
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    new RedBlock(Canvas1, 60*j, 26 * i);
+                }
+            }
+            timer.Tick += timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(999);
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                new RedBlock(Canvas1, 60 * j, 0);
+            }
+        }
+    }
+}
---
+++ Block Break game(sangjin)/RedBlock.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Windows;
+
+namespace Block_Break_Game
+{
+    class RedBlock //RedBlock Class 생성
+    {
+        Image RedBlock_img = new Image(); //이미지 파일 생성
+        private Canvas canvasA; //canvas 선언
+        DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        public Point pos
+        {
+            get
+            {
+                return new Point(
+                    Canvas.GetLeft(RedBlock_img),
+                    Canvas.GetTop(RedBlock_img)
+                );
+            }
+            set
+            {
+                Canvas.SetLeft(RedBlock_img, value.X);
+                Canvas.SetTop(RedBlock_img, value.Y);
+            }
+        }//RedBlock 좌표 설정
+
+
+
+        public RedBlock(Canvas canvas, double x, double y)
+        {
+            this.canvasA = canvas;
+            pos = new Point(x, y);
+
+            RedBlock_img.Source = new BitmapImage(
+                   new Uri(@"/images/red block.png", UriKind.RelativeOrAbsolute)
+               );
+
+            RedBlock_img.Width = 59;
+            canvas.Children.Add(RedBlock_img);
+
+            timer.Tick += timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Start();
+
+        }//Red Block 좌표 설정
+        void timer_Tick(object sender, EventArgs e)
+        {
+            pos = new Point(pos.X, pos.Y + 26);
+        }//밑으로 내려오는 움직임
+    }
+}
